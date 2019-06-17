46fe832f76e619f8721f4a4e0e43dbaf62986739
Adam Jay Lee <adamjlee96@gmail.com>
2019-06-17 16:37:05 -0400

    initial files

---
+++ ScreenCPY/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tesseract;
+
+namespace ScreenCPY
+{
+    public partial class Form1 : Form
+    {
+        //These variables control the mouse position
+        int selectX;
+        int selectY;
+        int selectWidth;
+        int selectHeight;
+        public Pen selectPen;
+
+        //This variable control when you start the right click
+        bool start = false;
+        public Form1()
+        {
+            InitializeComponent();
+            //Hide the Form
+            this.Hide();
+            //Create the Bitmap
+            Bitmap printscreen = new Bitmap(Screen.PrimaryScreen.Bounds.Width,
+                                     Screen.PrimaryScreen.Bounds.Height);
+            //Create the Graphic Variable with screen Dimensions
+            Graphics graphics = Graphics.FromImage(printscreen as Image);
+            //Copy Image from the screen
+            graphics.CopyFromScreen(0, 0, 0, 0, printscreen.Size);
+            //Create a temporal memory stream for the image
+            using (MemoryStream s = new MemoryStream())
+            {
+                //save graphic variable into memory
+                printscreen.Save(s, System.Drawing.Imaging.ImageFormat.Bmp);
+                pictureBox1.Size = new System.Drawing.Size(this.Width, this.Height);
+                //set the picture box with temporary stream
+                pictureBox1.Image = Image.FromStream(s);
+            }
+            //Show Form
+            this.Show();
+            //Cross Cursor
+            Cursor = Cursors.Cross;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            //validate if there is an image
+            if (pictureBox1.Image == null)
+                return;
+            //validate if right-click was trigger
+            if (start)
+            {
+                //refresh picture box
+                pictureBox1.Refresh();
+                //set corner square to mouse coordinates
+                selectWidth = e.X - selectX;
+                selectHeight = e.Y - selectY;
+                //draw dotted rectangle
+                pictureBox1.CreateGraphics().DrawRectangle(selectPen,
+                          selectX, selectY, selectWidth, selectHeight);
+            }
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            //validate when user right-click
+            if (!start)
+            {
+                if (e.Button == System.Windows.Forms.MouseButtons.Left)
+                {
+                    //starts coordinates for rectangle
+                    selectX = e.X;
+                    selectY = e.Y;
+                    selectPen = new Pen(Color.Red, 1);
+                    selectPen.DashStyle = DashStyle.DashDotDot;
+                }
+                //refresh picture box
+                pictureBox1.Refresh();
+                //start control variable for draw rectangle
+                start = true;
+            }
+            
+        }
+        private void pictureBox1_MouseUp(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (start)
+            {
+                //validate if there is image
+                if (pictureBox1.Image == null)
+                    return;
+                //same functionality when mouse is over
+                if (e.Button == System.Windows.Forms.MouseButtons.Left)
+                {
+                    pictureBox1.Refresh();
+                    selectWidth = e.X - selectX;
+                    selectHeight = e.Y - selectY;
+                    pictureBox1.CreateGraphics().DrawRectangle(selectPen, selectX,
+                             selectY, selectWidth, selectHeight);
+
+                }
+                start = false;
+                //function save image to clipboard
+                SaveToClipboard();
+            }
+        }
+
+        private void SaveToClipboard()
+        {
+            //validate if something selected
+            if (selectWidth > 0)
+            {
+
+                Rectangle rect = new Rectangle(selectX, selectY, selectWidth, selectHeight);
+                //create bitmap with original dimensions
+                Bitmap OriginalImage = new Bitmap(pictureBox1.Image, pictureBox1.Width, pictureBox1.Height);
+                //create bitmap with selected dimensions
+                Bitmap _img = new Bitmap(selectWidth, selectHeight);
+                //create graphic variable
+                Graphics g = Graphics.FromImage(_img);
+                //set graphic attributes
+                g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                g.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.HighQuality;
+                g.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+                g.DrawImage(OriginalImage, 0, 0, rect, GraphicsUnit.Pixel);
+                //insert image stream into clipboard
+                _img = sharpen(_img);
+                
+                var ocr = new TesseractEngine("./tessdata", "eng",EngineMode.TesseractAndCube);
+                var page = ocr.Process(_img);
+                Console.WriteLine("Copied: " + page.GetText());
+
+                string s = page.GetText();
+                if (!String.IsNullOrEmpty(s))
+                {
+                    Clipboard.SetText(s);
+                    Clipboard.SetImage(_img);
+                }
+            }
+            //End application
+            Application.Exit();
+        }
+
+        public static Bitmap sharpen(Bitmap image)
+        {
+            Bitmap sharpenImage = new Bitmap(image.Width, image.Height);
+
+            int filterWidth = 3;
+            int filterHeight = 3;
+            int w = image.Width;
+            int h = image.Height;
+
+            double[,] filter = new double[filterWidth, filterHeight];
+
+            filter[0, 0] = filter[0, 1] = filter[0, 2] = filter[1, 0] = filter[1, 2] = filter[2, 0] = filter[2, 1] = filter[2, 2] = -1;
+            filter[1, 1] = 9;
+
+            double factor = 1.0;
+            double bias = 0.0;
+
+            Color[,] result = new Color[image.Width, image.Height];
+
+            for (int x = 0; x < w; ++x)
+            {
+                for (int y = 0; y < h; ++y)
+                {
+                    double red = 0.0, green = 0.0, blue = 0.0;
+
+                    for (int filterX = 0; filterX < filterWidth; filterX++)
+                    {
+                        for (int filterY = 0; filterY < filterHeight; filterY++)
+                        {
+                            int imageX = (x - filterWidth / 2 + filterX + w) % w;
+                            int imageY = (y - filterHeight / 2 + filterY + h) % h;
+
+                            Color imageColor = image.GetPixel(imageX, imageY);
+
+                            red += imageColor.R * filter[filterX, filterY];
+                            green += imageColor.G * filter[filterX, filterY];
+                            blue += imageColor.B * filter[filterX, filterY];
+                        }
+                        int r = Math.Min(Math.Max((int)(factor * red + bias), 0), 255);
+                        int g = Math.Min(Math.Max((int)(factor * green + bias), 0), 255);
+                        int b = Math.Min(Math.Max((int)(factor * blue + bias), 0), 255);
+
+                        result[x, y] = Color.FromArgb(r, g, b);
+                    }
+                }
+            }
+            for (int i = 0; i < w; ++i)
+            {
+                for (int j = 0; j < h; ++j)
+                {
+                    sharpenImage.SetPixel(i, j, result[i, j]);
+                }
+            }
+            return sharpenImage;
+        }
+    }
+}
